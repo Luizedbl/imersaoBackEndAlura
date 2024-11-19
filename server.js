2c1f71cab299c565cef50cc74e1302fbe43fea1e
Luiz Eduardo <luizeb.lima@gmail.com>
2024-11-19 11:27:29 -0300

    iniciando manipulação de dados

--- server.js
+++ server.js
@@ -1,30 +1,12 @@
 import express from "express";
 
 const posts = [
-    {
-      descricao: "Uma foto teste",
-      imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-      descricao: "Gato fazendo yoga",
-      imagem: "https://placecats.com/yoga/400/200"
-    },
-    {
-      descricao: "Gato explorando a caixa",
-      imagem: "https://placecats.com/box/500/300"
-    },
-    {
-      descricao: "Gato dormindo em um teclado",
-      imagem: "https://placecats.com/keyboard/600/400"
-    },
-    {
-      descricao: "Gato olhando pela janela",
-      imagem: "https://placecats.com/window/700/500"
-    },
-    {
-      descricao: "Gato brincando com um novelo de lã",
-      imagem: "https://placecats.com/yarn/800/600"
-    }
+    {id:1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150"},
+    {id:2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/yoga/400/200"},
+    {id:3, descricao: "Gato explorando a caixa",nimagem: "https://placecats.com/box/500/300"},
+    {id:4, descricao: "Gato dormindo em um teclado", imagem: "https://placecats.com/keyboard/600/400"},
+    {id:5, descricao: "Gato olhando pela janela", imagem: "https://placecats.com/window/700/500"},
+    {id:6, descricao: "Gato brincando", imagem: "https://placecats.com/yarn/800/600"}
 ];
 
 const app = express();
@@ -33,7 +15,17 @@ app.listen(3000, () => {
     console.log("Servidor Escutando.");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Rota inicial");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
 
+function buscarPostPorID(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+};
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id);
+    res.status(200).json(posts[index]);
+});
